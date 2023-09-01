5743db9e66900261ab16aadff3edb14b3e460389
ahmadzulfadli <ponyongekruso34@gmail.com>
2023-09-01 15:19:14 +0700

    finish CRUD

--- fileapi/src/main/java/com/file/api/fileapi/controller/FileStorageController.java
+++ fileapi/src/main/java/com/file/api/fileapi/controller/FileStorageController.java
@@ -1,11 +1,14 @@
 package com.file.api.fileapi.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +23,7 @@ public class FileStorageController {
     @Autowired
     private FileStorageService fileStorageService;
 
+    // CREATE -------------------------------------------------------------------
     @PostMapping("/upload")
     public WebResponse<FileResponse> uploadFile(@RequestParam("file") MultipartFile file) throws IOException{
 
@@ -30,4 +34,47 @@ public class FileStorageController {
         return webResponse;
 
     }
+
+    // READ -------------------------------------------------------------------
+    @GetMapping("/data/{id}")
+    public WebResponse<FileResponse> getFile(@PathVariable String id){
+        FileResponse fileResponse = fileStorageService.getFile(id);
+
+        WebResponse<FileResponse> webResponse = WebResponse.<FileResponse>builder()
+                    .data(fileResponse)
+                    .build();
+        return webResponse;
+    }
+    @GetMapping("/data")
+    public WebResponse<List<FileResponse>> getAllData(){
+        List<FileResponse> fileResponse = fileStorageService.getAllData();
+
+        WebResponse<List<FileResponse>> webResponse = WebResponse.<List<FileResponse>>builder()
+                    .data(fileResponse)
+                    .build();
+        return webResponse;
+    }
+
+    // UPDATE -------------------------------------------------------------------
+    @PatchMapping("/update/{id}")
+    public WebResponse<FileResponse> updateFile(@PathVariable String id,@RequestParam("file") MultipartFile file) throws IOException{
+
+        FileResponse fileResponse = fileStorageService.updateFile(id, file);
+        WebResponse<FileResponse> webResponse = WebResponse.<FileResponse>builder()
+                .data(fileResponse)
+                .build();
+        return webResponse;
+
+    }
+    // DELETE -------------------------------------------------------------------
+    @DeleteMapping("/data/{id}")
+    public WebResponse<String> deleteData(@PathVariable String id) throws IOException{
+        fileStorageService.deleteData(id);
+
+        WebResponse<String> webResponse = WebResponse.<String>builder()
+                    .data("Sukses deleted data id : " + id)
+                    .build();
+        return webResponse;
+    }
+
 }
--- fileapi/src/main/java/com/file/api/fileapi/service/FileStorageService.java
+++ fileapi/src/main/java/com/file/api/fileapi/service/FileStorageService.java
@@ -1,17 +1,23 @@
 package com.file.api.fileapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.file.api.fileapi.entity.File;
@@ -25,7 +31,8 @@ public class FileStorageService {
     @Autowired
     private FileRepository fileRepository;
 
-    public FileResponse toFileResponse(File file) {
+    // Fungsi Tambahan
+    private FileResponse toFileResponse(File file) {
         return FileResponse.builder()
             .id(file.getId())
             .nameFile(file.getNameFile())
@@ -33,6 +40,32 @@ public class FileStorageService {
             .size(file.getSize())
             .build();
     }
+
+    private String saveFile(MultipartFile file, String uploadDir, String fileName) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = file.getInputStream()) {
+            Path path = uploadPath.resolve(fileName);
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+            URI uri = URI.create(path.toString());
+            String filePath = uri.getPath();
+            return filePath;
+        }
+    }
+
+    private void deleteFile(String path) throws IOException{
+        Path filePath = Paths.get(path);
+        if (Files.exists(filePath)){
+            Files.delete(filePath);
+        }else{
+            throw new FileNotFoundException("File not found at path" + path);
+        }
+    }
+
 
     // CREATE -------------------------------------------------------------------
     @Transactional
@@ -53,18 +86,57 @@ public class FileStorageService {
         return toFileResponse(file);
     }
 
-    private String saveFile(MultipartFile file, String uploadDir, String fileName) throws IOException {
-        Path uploadPath = Paths.get(uploadDir);
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
-        }
+    // READ -------------------------------------------------------------------
+    @Transactional(readOnly = true)
+    public FileResponse getFile(String id){
 
-        try (InputStream inputStream = file.getInputStream()) {
-            Path filePath = uploadPath.resolve(fileName);
-            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-            return filePath.toUri().toString();
+        File file = fileRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Data not found"));
+
+        return toFileResponse(file);
+    }
+
+    @Transactional(readOnly = true)
+    public List<FileResponse> getAllData(){
+        List<File> file = fileRepository.findAll();
+        return file.stream().map(this::toFileResponse).toList();
+    }
+
+    // UPDATE -------------------------------------------------------------------
+    @Transactional
+    public FileResponse updateFile(String fileId, MultipartFile request) throws IOException {
+        Optional<File> optionalFile = fileRepository.findById(fileId);
+        
+        if (optionalFile.isPresent()) {
+            File file = optionalFile.get();
+
+            deleteFile(file.getUrlFile());
+            
+            String uploadDir = "/home/pr001/Desktop/SpringBoot/upload";
+            String fileName = StringUtils.cleanPath(request.getOriginalFilename());
+            long size = request.getSize();
+            String fileUrl = saveFile(request, uploadDir, fileName);
+
+            // Update file properties
+            file.setNameFile(fileName);
+            file.setUrlFile(fileUrl);
+            file.setSize(size);
+
+            fileRepository.save(file);
+
+            return toFileResponse(file);
+        } else {
+            throw new FileNotFoundException("File not found with ID: " + fileId);
         }
     }
 
+    // DELETE -------------------------------------------------------------------
+    @Transactional
+    public void deleteData(String id) throws IOException{
+        File file = fileRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"));
+        deleteFile(file.getUrlFile());
+        fileRepository.delete(file);
+    }
 }
 
